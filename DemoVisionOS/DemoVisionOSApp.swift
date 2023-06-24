90c346916fc7486f66089852c38bc72300e26ab3
Devendra Kumar <devendra.kumar@nagarro.com>
2023-06-24 21:12:37 +0530

    Initial Commit

---
+++ DemoVisionOS/DemoVisionOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoVisionOSApp.swift
+//  DemoVisionOS
+//
+//  Created by Devendra Agnihotri on 24/06/23.
+//
+
+import SwiftUI
+
+@main
+struct DemoVisionOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
