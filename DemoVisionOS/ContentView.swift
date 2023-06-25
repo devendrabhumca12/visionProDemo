9fe8448424f8865f1f043439112b136bf10832b2
Devendra Kumar <devendra.kumar@nagarro.com>
2023-06-25 10:22:23 +0530

    Initial commit

--- DemoVisionOS/ContentView.swift
+++ DemoVisionOS/ContentView.swift
@@ -8,22 +8,45 @@
 import SwiftUI
 import RealityKit
 import RealityKitContent
+struct Member: Identifiable {
+    var id = UUID()
+    var name: String
+    var position: String
+    var mobileNumber: String
+}
 
 struct ContentView: View {
+    var biraTeam: [Member] = [
+        Member(name: "Mani", position: "Architect", mobileNumber: "+1 123-456-7890"),
+        Member(name: "Dhanesh", position: "Manager", mobileNumber: "+1 987-654-3210"),
+        Member(name: "Ganesh", position: "iOS Developer", mobileNumber: "+1 987-654-3210"),
+        Member(name: "Sumit", position: "iOS Developer", mobileNumber: "+1 987-654-3210"),
+        Member(name: "Akshay Gupta", position: "iOS Developer", mobileNumber: "+1 987-654-3210"),
+        Member(name: "Sawan Rana", position: "iOS Developer", mobileNumber: "+1 987-654-3210"),
+        Member(name: "Divya", position: "iOS Developer", mobileNumber: "+1 987-654-3210"),
+        Member(name: "Devendra", position: "iOS Developer", mobileNumber: "+1 987-654-3210"),
+    ]
     var body: some View {
         NavigationSplitView {
             List {
-                Text("Item")
+                Text("Bira iOS Team")
             }
             .navigationTitle("Sidebar")
         } detail: {
             VStack {
-                Model3D(named: "Scene", bundle: realityKitContentBundle)
-                    .padding(.bottom, 50)
-
-                Text("Hello, world!")
+                List(biraTeam) { member in
+                    HStack(alignment: .top) {
+                        Text(member.name)
+                            .font(.headline)
+                        Text(member.position)
+                            .font(.subheadline)
+                        Text(member.mobileNumber)
+                            .font(.subheadline)
+                            .foregroundColor(.gray)
+                    }
+                }
             }
-            .navigationTitle("Content")
+            .navigationTitle("iOS Team Details")
             .padding()
         }
     }
@@ -32,3 +55,26 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+
+
+
+//struct ContentView: View {
+//   
+//    
+//    var body: some View {
+//        NavigationView {
+//            List(biraTeam) { member in
+//                VStack(alignment: .leading) {
+//                    Text(member.name)
+//                        .font(.headline)
+//                    Text(member.position)
+//                        .font(.subheadline)
+//                    Text(member.mobileNumber)
+//                        .font(.subheadline)
+//                        .foregroundColor(.gray)
+//                }
+//            }
+//            .navigationBarTitle("BIRA Team")
+//        }
+//    }
+//}
